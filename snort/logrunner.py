9f99f2b61d4a01a7303ddf887035879989e3165c
Simon Jouet <simon.jouet@gmail.com>
2014-09-23 14:38:43 +0100

    Snort inline IPS

---
+++ snort/logrunner.py
@@ -0,0 +1,32 @@
+import time
+import sys
+import os
+import httplib
+
+def postNotification(notification):
+	conn = httplib.HTTPConnection("172.17.42.1")
+	conn.request("POST", "/notification", notification)
+	conn.close()
+
+def tail(file):
+	interval = 1.0
+	while True:
+		cursor = file.tell()
+		line = file.readline()
+		if not line:
+			time.sleep(interval)
+			file.seek(cursor)
+		else:
+			yield line
+
+
+with open(sys.argv[1]) as f:
+	chunk = []
+	for line in tail(f):
+		if line != os.linesep:
+			chunk.append(line)
+		else:
+			alert = ''.join(chunk)
+			postNotification(alert)
+			print alert
+			chunk = []
